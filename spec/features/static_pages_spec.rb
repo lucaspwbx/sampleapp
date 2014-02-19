4f28272657fea0170d7455687a876d2f351004c6
Lucas Weiblen <lucasweiblen@gmail.com>
2014-02-18 22:33:14 -0300

    Finish static pages

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+
+  describe 'Home page' do
+
+    before do
+      visit '/static_pages/home'
+    end
+
+    it "have the content 'Sample App'" do
+      expect(page).to have_content('Sample App')
+    end
+
+    it 'has the title home' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe 'Help page' do
+    before { visit '/static_pages/help' }
+
+    it 'has the content help' do
+      expect(page).to have_content('Help')
+    end
+
+    it 'has the title help' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe 'About page' do
+    before { visit '/static_pages/about' }
+
+    it 'has the content about us' do
+      expect(page).to have_content('About Us')
+    end
+
+    it 'has the title about us' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+  end
+end
