9266f0f07a730f559253ec265e1b46b01bdb523d
Lucas Weiblen <lucasweiblen@gmail.com>
2014-02-22 21:47:11 -0300

    Finish user edit, update, index and destroy actions

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,8 +1,55 @@
 require 'spec_helper'
 
-describe 'User pages' do
+describe 'User pages', type: :request do
 
   subject { page }
+
+  describe 'index' do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: 'Bob', email: 'bob@example.com')
+      FactoryGirl.create(:user, name: 'Ben', email: 'ben@example.com')
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe 'pagination' do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all) { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it 'should list each user' do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe 'delete links' do
+
+      it { should_not have_link('delete') }
+
+      describe 'as an admin user' do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it 'should be able to delete another user' do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
 
   describe 'signup page' do
     before { visit signup_path }
@@ -50,5 +97,112 @@ describe 'User pages' do
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+  end
+
+  describe 'edit' do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe 'page' do
+      it { should have_content('Update your profile') }
+      it { should have_title('Edit user') }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+
+      describe 'with invalid information' do
+        before { click_button 'Save changes' }
+
+        it { should have_content('error') }
+      end
+
+      describe 'with valid information' do
+        let(:new_name) { 'New Name' }
+        let(:new_email) { 'new@example.com' }
+        before do
+          fill_in 'Name', with: new_name
+          fill_in 'Email', with: new_email
+          fill_in 'Password', with: user.password
+          fill_in 'Confirm Password', with: user.password
+          click_button 'Save changes'
+        end
+
+        it { should have_title(new_name) }
+        it { should have_selector('.alert-success') }
+        it { should have_link('Sign out', href: signout_path) }
+        specify { expect(user.reload.name).to eq new_name }
+        specify { expect(user.reload.email).to eq new_email }
+      end
+    end
+  end
+
+  describe 'authorization' do
+
+    describe 'as non-admin user' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe 'submitting a DELETE request' do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
+
+    describe 'for non-signed-in users' do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe 'when attempting to visit a protected page' do
+        before do
+          visit edit_user_path(user)
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: user.password
+          click_button 'Sign in'
+        end
+
+        describe 'after signing in' do
+          it 'should render the desired protected page' do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
+      describe 'in the Users controller' do
+
+        describe 'visiting the edit page' do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe 'submitting to the update action' do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'visiting the user index' do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+      end
+    end
+
+    describe 'as wrong user' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: 'wrong@example.com') }
+      before { sign_in user, no_capybara: true }
+
+      describe 'submitting a GET request to the Users#edit action' do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+
+      describe 'submitting a PATCH request to the Users#update action' do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
   end
 end
